dded96d414370a8714f8572c7bba00357da6e8f3
Pedro Costa <pedrocostavgs@gmail.com>
2025-03-21 16:25:04 -0300

    fix: change not found syle directory

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,9 +1,8 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import React from 'react';
+import styles from "./styles/notFoundStyle"
 
 export default function NotFoundScreen() {
   return (
@@ -18,16 +17,3 @@ export default function NotFoundScreen() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
